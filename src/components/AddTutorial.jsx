1041e1671441ac96d6291d8f4949e366232c1e9d
6019thinkingman <99737495+6019thinkingman@users.noreply.github.com>
2022-07-30 22:27:34 +0300

    structure created

---
+++ src/components/AddTutorial.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+
+const AddTutorial = () => {
+    const [title, setTitle] = useState()
+    const [desc,setDesc]= useState()
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // return(
+        //     // alert({title , desc})
+        // )
+    }
+    const handleTitleChange = (e) => {
+        setTitle(e.target.value)
+    }
+
+    const handleDescChange = (e) => {
+        console.log(e.target.value)
+        setDesc(e.target.value)
+    }
+
+  return (
+    <div>
+      <h1>Add Your Tutorial</h1>
+      <Form >
+        <Form.Group className="mb-4" controlId="formBasicTitle">
+          <Form.Label>Title</Form.Label>
+          <br />
+          <Form.Control  type="text" placeholder="Enter Your Title" onChange={handleTitleChange} /> <br />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicDesc">
+          <Form.Label>Description</Form.Label>
+          <br />
+          <Form.Control type="text" placeholder="Enter Your Description" onChange={handleDescChange} />
+          <br />
+        </Form.Group>
+
+        <Button variant="primary" type="submit" onClick={handleSubmit}>
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
+};
+
+export default AddTutorial;
---
+++ src/components/TutorialList.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TutorialList = () => {
+  return (
+    <div>
+        tutorial list
+
+    </div>
+  )
+}
+
+export default TutorialList
